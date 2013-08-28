e3861dcd26900ec0bf1b931a10427163f2bc1dc3
Tom Leslie <tom@fuzzproductions.com>
2013-08-28 14:26:09 -0400

    Creating the base php file and adding the necessary comments.

---
+++ credlyLogin.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Plugin Name: Credly Login
+ * Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+ * Description: Allows users to create a Wordpress account by signing in with an existing Credly account.
+ * Version: 1.0
+ * Author: Fuzz Productions
+ * Author URI: http://fuzzproductions.com
+ * License: GPL2
+ *
+ * Copyright 2013  Fuzz Productions  (email : contact@fuzzproductions.com)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
